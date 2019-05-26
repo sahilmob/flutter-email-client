e861aeee6120c015b521c133feabe257b4afdd42
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-26 14:59:58 +0300

    use message list widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "./MessageList.dart";
 
 void main() => runApp(EmailApp());
 
@@ -8,40 +9,7 @@ class EmailApp extends StatelessWidget {
     return MaterialApp(
       title: "Flutter Demo",
       theme: ThemeData(primarySwatch: Colors.blueGrey, accentColor: Colors.red),
-      home: MyHomePage(title: "Flutter Demo Home Page"),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  var messsages = const [];
-  MyHomePage({this.title});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: ListView.separated(
-        itemCount: messsages.length,
-        separatorBuilder: (BuildContext context, int index) => Divider(),
-        itemBuilder: (BuildContext context, int index) {
-          var message = messsages[index];
-          return ListTile(
-            title: Text(message['subject']),
-            subtitle: Text(
-              message['body'],
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            isThreeLine: true,
-            leading: CircleAvatar(
-              child: Text("SM"),
-            ),
-          );
-        },
-      ),
+      home: MessageList(title: "Flutter Demo Home Page"),
     );
   }
 }
