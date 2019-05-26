315fb51440bce6a605592c6e372755f72e882af8
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-26 14:40:41 +0300

    create basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() => runApp(EmailApp());
 
@@ -6,15 +6,22 @@ class EmailApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Flutter Demo",
       theme: ThemeData(primarySwatch: Colors.blueGrey, accentColor: Colors.red),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: "Flutter Demo Home Page"),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
+  var messsages = const [
+    "My first message",
+    "My second message",
+    "You won lottery",
+    "You should read this message",
+    "You won lottery again",
+  ];
   MyHomePage({this.title});
   @override
   Widget build(BuildContext context) {
@@ -22,8 +29,21 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        child: Text("Hello"),
+      body: ListView.separated(
+        itemCount: messsages.length,
+        separatorBuilder: (BuildContext context, int index) => Divider(),
+        itemBuilder: (BuildContext context, int index) {
+          var title = messsages[index];
+          return ListTile(
+            title: Text(title),
+            subtitle: Text(
+                "Subtitle which is very very long so everyone can read it, seet it"),
+            isThreeLine: true,
+            leading: CircleAvatar(
+              child: Text("SM"),
+            ),
+          );
+        },
       ),
     );
   }
