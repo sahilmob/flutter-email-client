6415ccaea0b7544fb9871002406f6a5fa508f049
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-27 18:16:59 +0300

    use TextFormInput

--- lib/MessageCompose.dart
+++ lib/MessageCompose.dart
@@ -12,6 +12,8 @@ class _MessageComposeState extends State<MessageCompose> {
   String subject;
   String body;
 
+  final GlobalKey<FormState> key = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,45 +21,48 @@ class _MessageComposeState extends State<MessageCompose> {
         title: Text("Compose New Message"),
       ),
       body: SingleChildScrollView(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          ListTile(
-            title: TextField(
-                decoration: InputDecoration(
-                  labelText: "To",
-                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                onChanged: (value) => to = value),
-          ),
-          ListTile(
-            title: TextField(
-                decoration: InputDecoration(
-                  labelText: "Subject",
-                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                onChanged: (value) => subject = value),
-          ),
-          ListTile(
-            title: TextField(
-              decoration: InputDecoration(
-                labelText: "Body",
-                labelStyle: TextStyle(fontWeight: FontWeight.bold),
+          child: Form(
+        key: key,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            ListTile(
+              title: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "To",
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  onSaved: (value) => to = value),
+            ),
+            ListTile(
+              title: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Subject",
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  onSaved: (value) => subject = value),
+            ),
+            ListTile(
+              title: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Body",
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  maxLines: 8,
+                  onSaved: (value) => body = value),
+            ),
+            ListTile(
+              title: RaisedButton(
+                child: Text("Send"),
+                onPressed: () {
+                  this.key.currentState.save();
+                  Message message = Message(subject: subject, body: body);
+                  Navigator.pop(context, message);
+                },
               ),
-              maxLines: 8,
-              onChanged: (value) => body = value,
-            ),
-          ),
-          ListTile(
-            title: RaisedButton(
-              child: Text("Send"),
-              onPressed: () {
-                Message message = Message(subject: subject, body: body);
-                Navigator.pop(context, message);
-              },
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       )),
     );
   }
