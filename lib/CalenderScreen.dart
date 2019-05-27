bb784aab43757f1e38b2bb1f0a08a109f3d629e2
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-27 23:25:38 +0300

    add scaffold, appbar, and drawer

--- lib/CalenderScreen.dart
+++ lib/CalenderScreen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import "./AppDrawer.dart";
 
 class CalenderScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Calender screen"),
+      appBar: AppBar(
+        title: Text("Calender"),
+      ),
+      drawer: AppDrawer(),
+      body: Center(
+        child: Text("Calender screen"),
+      ),
     );
   }
 }
--- lib/ContactsScreen.dart
+++ lib/ContactsScreen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import "./AppDrawer.dart";
 
 class ContactsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Contacts screen"),
+      appBar: AppBar(
+        title: Text("Contacts"),
+      ),
+      drawer: AppDrawer(),
+      body: Center(
+        child: Text("Contacts screen"),
+      ),
     );
   }
 }
