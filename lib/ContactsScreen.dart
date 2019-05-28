096ba02f541a577d40fd83e7ea8f79959a0087a9
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-28 18:42:00 +0300

    build UI from contacts stream

--- lib/ContactsScreen.dart
+++ lib/ContactsScreen.dart
@@ -1,17 +1,54 @@
 import 'package:flutter/material.dart';
+
 import "./AppDrawer.dart";
+import "./ContactManager.dart";
 
 class ContactsScreen extends StatelessWidget {
+  ContactManager manager = ContactManager();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Contacts"),
+    return DefaultTabController(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Contacts"),
+          actions: <Widget>[
+            Chip(
+              backgroundColor: Colors.red,
+              label: StreamBuilder<int>(
+                stream: manager.contactCounter,
+                builder: (BuildContext context, snapshot) {
+                  return Text(
+                    (snapshot.data ?? 0).toString(),
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 16),
+            )
+          ],
+        ),
+        drawer: AppDrawer(),
+        body: StreamBuilder<List<String>>(
+          stream: manager.contactListNow,
+          builder: (BuildContext context, snapshot) {
+            List<String> contacts = snapshot.data;
+            return ListView.separated(
+              itemCount: contacts.length,
+              itemBuilder: (BuildContext context, index) => ListTile(
+                    title: Text(contacts[index]),
+                  ),
+              separatorBuilder: (BuildContext context, int index) {
+                return Divider();
+              },
+            );
+          },
+        ),
       ),
-      drawer: AppDrawer(),
-      body: Center(
-        child: Text("Contacts screen"),
-      ),
+      length: 2,
     );
   }
 }
