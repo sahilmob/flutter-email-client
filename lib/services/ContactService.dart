b92d1fb3c171ad0905fc4385888be6b39981a1f2
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-04 18:29:43 +0300

    filter contact if query is provided

--- lib/services/ContactService.dart
+++ lib/services/ContactService.dart
@@ -5,12 +5,16 @@ import "../models/Contact.dart";
 
 class ConactService {
   static String _url = "https://jsonplaceholder.typicode.com/users";
-  static Future browse() async {
+
+  static Future<List<Contact>> browse({query}) async {
     http.Response response = await http.get(_url);
     String contacts = response.body;
     List collection = json.decode(contacts);
-    List<Contact> _contacts =
-        collection.map((m) => Contact.fromJson(m)).toList();
-    return _contacts;
+    Iterable<Contact> _contacts = collection.map((m) => Contact.fromJson(m));
+    if (query != null && query.isNotEmpty) {
+      _contacts = _contacts
+          .where((contact) => contact.name.toLowerCase().contains(query));
+    }
+    return _contacts.toList();
   }
 }
