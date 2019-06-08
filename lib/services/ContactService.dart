8ae18f43eccbfd9a883cf9d3a2ea49be28e17174
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-08 19:30:09 +0300

    change api url and remove client side filtering

--- lib/services/ContactService.dart
+++ lib/services/ContactService.dart
@@ -4,17 +4,15 @@ import "dart:convert";
 import "../models/Contact.dart";
 
 class ConactService {
-  static String _url = "https://jsonplaceholder.typicode.com/users";
+  static String _url =
+      "https://flutter-email-client-backend.herokuapp.com/contacts";
 
   static Future<List<Contact>> browse({query}) async {
-    http.Response response = await http.get(_url);
+    http.Response response = await http.get("$_url?q=$query");
     String contacts = response.body;
     List collection = json.decode(contacts);
     Iterable<Contact> _contacts = collection.map((m) => Contact.fromJson(m));
-    if (query != null && query.isNotEmpty) {
-      _contacts = _contacts
-          .where((contact) => contact.name.toLowerCase().contains(query));
-    }
+
     return _contacts.toList();
   }
 }
