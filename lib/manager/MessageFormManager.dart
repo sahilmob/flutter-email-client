b4b62d102135f401bb35faac07bdf539be7b1090
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-26 19:04:33 +0300

    validate email input

--- lib/manager/MessageFormManager.dart
+++ lib/manager/MessageFormManager.dart
@@ -1,12 +1,22 @@
+import 'dart:async';
+
 import 'package:rxdart/subjects.dart';
 
 class MessageFormManager {
-  final _email = BehaviorSubject<String>.seeded("");
+  final _email = BehaviorSubject<String>.seeded("@");
 
-  Stream<String> get email$ => _email.stream;
+  Stream<String> get email$ => _email.stream.transform(validateEmail);
   Sink<String> get inEmail => _email.sink;
 
-  MessageFormManager() {
-    _email.listen((value) => print(value));
-  }
+  static bool isEmail(String email) => email.contains("@");
+
+  final validateEmail =
+      StreamTransformer<String, String>.fromHandlers(handleData: (value, sink) {
+    if (isEmail(value)) {
+      print("isemail");
+      sink.add(value);
+    } else {
+      sink.addError("Our message error");
+    }
+  });
 }
