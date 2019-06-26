b7770a2e4094abf4d89cad630a394454cfea23e4
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-26 16:30:55 +0300

    init message form manager

---
+++ lib/manager/MessageFormManager.dart
@@ -0,0 +1,12 @@
+import 'package:rxdart/subjects.dart';
+
+class MessageFormManager {
+  final _email = BehaviorSubject<String>.seeded("");
+
+  Stream<String> get email$ => _email.stream;
+  Sink<String> get inEmail => _email.sink;
+
+  MessageFormManager() {
+    _email.listen((value) => print(value));
+  }
+}
