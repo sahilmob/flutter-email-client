3525e2f3df71f2b6f233cf090b158adee85693d1
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-30 22:57:06 +0300

    move validation to a mixin

--- lib/manager/MessageFormManager.dart
+++ lib/manager/MessageFormManager.dart
@@ -1,22 +1,11 @@
 import 'dart:async';
 
+import 'package:emailapp/manager/validation.dart';
 import 'package:rxdart/subjects.dart';
 
-class MessageFormManager {
+class MessageFormManager with Validation {
   final _email = BehaviorSubject<String>.seeded("@");
 
   Stream<String> get email$ => _email.stream.transform(validateEmail);
   Sink<String> get inEmail => _email.sink;
-
-  static bool isEmail(String email) => email.contains("@");
-
-  final validateEmail =
-      StreamTransformer<String, String>.fromHandlers(handleData: (value, sink) {
-    if (isEmail(value)) {
-      print("isemail");
-      sink.add(value);
-    } else {
-      sink.addError("Our message error");
-    }
-  });
 }
